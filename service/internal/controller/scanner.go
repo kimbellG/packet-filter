07038147f95ecffdf2d7b741428481804bd82c2a
Filippenkov Artyom <filippenkov95@gmail.com>
2021-09-10 11:20:09 +0300

    added controller interactor for xdp filter

--- service/internal/controller/scanner.go
+++ service/internal/controller/scanner.go
@@ -5,6 +5,42 @@ import "context"
 //Scanner is interface for communicative beetween controller and package scanner
 type Scanner interface {
 	Count() Count
+	Filter() Filter
+}
+
+type IPProto int
+
+const (
+	IP IPProto = iota
+	ICMP
+	IGMP
+	IPIP
+	TCP
+	EGP
+	PUP
+	UDP
+	IDP
+	TP
+	DCCP
+	IPV6
+	RSVP
+	GRE
+	ESP
+	AH
+	MTP
+	BEETPH
+	ENCAP
+	PIM
+	COMP
+	SCTP
+	UDPLITE
+	MPLS
+	ETHERNET
+)
+
+type Filter interface {
+	Block(protocol IPProto) error
+	UnBlock(protocol IPProto) error
 }
 
 //Count is interface for count controll
